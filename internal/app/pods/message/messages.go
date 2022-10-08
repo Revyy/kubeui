226a05ffd2c5c31a4fe4b93908c0b292f323be98
Jonas Olander <jonas.olander91@gmail.com>
2022-10-09 01:22:13 +0200

    Add comments to exported types and functions

--- internal/app/pods/message/messages.go
+++ internal/app/pods/message/messages.go
@@ -2,26 +2,32 @@ package message
 
 import v1 "k8s.io/api/core/v1"
 
+// Initialization is sent after fetching the initial list of available namespaces.
 type Initialization struct {
 	NamespaceList *v1.NamespaceList
 }
 
+// NewInitialization creates a new Initialization message.
 func NewInitialization(namespaceList *v1.NamespaceList) Initialization {
 	return Initialization{NamespaceList: namespaceList}
 }
 
+// ListPods is used as the result of fetching a list of pods in the current namespace.
 type ListPods struct {
 	PodList *v1.PodList
 }
 
+// NewListPods creates a new ListPods message.
 func NewListPods(podList *v1.PodList) ListPods {
 	return ListPods{PodList: podList}
 }
 
+// PodDeleted is sent after a pod has been deleted.
 type PodDeleted struct {
 	Name string
 }
 
+// NewPodDeleted creates a new PodDeleted message.
 func NewPodDeleted(name string) PodDeleted {
 	return PodDeleted{Name: name}
 }
